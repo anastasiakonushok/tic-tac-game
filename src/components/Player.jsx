93d88ccdd6768cd5b53702d700f7beaf773aac05
anastasia konushok <work.an.kon@gmail.com>
2025-07-10 18:01:01 +0300

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,10 @@
+export default function Player() {
+    return
+    <li>
+        <span className="player">
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">0</span>
+        </span>
+        <button> Edit</button>
+    </li>;
+}
